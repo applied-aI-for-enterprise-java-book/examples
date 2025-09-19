5650130fb66ff3907e81586bc10d754cc4575a0c
Alex Soto <alexsoto@MacBook-Air-de-Alex.local>
2025-09-19 08:22:03 +0200

    Adds MCP usage

--- ch11/chinese-zodiac-cycle-mcp-client/src/test/java/org/acme/MCPClientTest.java
+++ ch11/chinese-zodiac-cycle-mcp-client/src/test/java/org/acme/MCPClientTest.java
@@ -1,13 +1,28 @@
 package org.acme;
 
 import dev.langchain4j.mcp.client.McpClient;
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
 import io.quarkiverse.langchain4j.mcp.runtime.McpClientName;
+import io.quarkiverse.langchain4j.mcp.runtime.McpToolBox;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class MCPClientTest {
+
+    /**@RegisterAiService
+    interface Assistant {
+
+        @SystemMessage("""
+                You are an assistant to answer questions for any user.
+                You have a function to calculate the chinese zodiacal animal from a given date. 
+                """)
+        @McpToolBox("zodiac")
+        String askMe(@UserMessage  String message);
+    }**/
 
     @Inject
     @McpClientName("zodiac")
